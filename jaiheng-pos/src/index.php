cbe7e4a1167b73a0a4cf2d55d8d0874317484ea9
Piyawat Sritavong <85154639+PiyawatSritavong@users.noreply.github.com>
2023-02-25 16:14:45 +0700

    Delete jaiheng-pos directory

--- jaiheng-pos/src/index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'public/header.php';?>
-    <center>
-        <div >
-            <img src="public/uplode/หลังขาว.png" class="img-index">
-        </div>
-        <a href="app/login/login.php"><button type="submit" name="login_Sale" class="bt-index"><span>Sale </span></button></a>
-		
-		<a href="app/login/login.php"><button type="submit" name="login_Sale" class="bt-index"><span>Admin </span></button></a>
-    </center>
--- jaiheng-pos/src/public/nav-pos.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php';?>
-<body>
-<div class="navbar">
-    <h1 style="color: #ffff;">username</h1>
-    <a href="http://localhost/1421-Project/jaiheng-pos/src/app/login/logout.php" style="width:100px;float:right;">Log Out</a>
-</div>
